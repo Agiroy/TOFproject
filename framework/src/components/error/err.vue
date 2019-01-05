8d5f64d91f11fb0b9870f56bebd82e0118b86d76
Your Name <you@example.com>
2019-01-05 23:01:30 +0800

    err[C页面提交

--- framework/src/components/error/err.vue
+++ framework/src/components/error/err.vue
@@ -1,5 +1,12 @@
 <template>
-    <div>
-        404
+    <div class="err">
+     <h2>404</h2>
     </div>
 </template>
+<style>
+        .err{
+            font-size: 1rem;
+            text-align:center;
+        }
+</style>
+       
