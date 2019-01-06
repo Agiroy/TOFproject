c3283537d5f85c423115123bfb2937a72b5be780
yjx <850411454@qq.com>
2019-01-06 20:42:30 +0800

    yjx1/6

--- framework/src/components/home/index.vue
+++ framework/src/components/home/index.vue
@@ -1,18 +1,30 @@
 <template>
     <div class="home">
-        <h2></h2>
+        <Homeheader-com/>
+        <Homebanner-com/>
+        <Homenav-com/>
     </div>
 </template>
 
 <script>
 
 import Vuex from "vuex";
+import Homeheader from "./components/homeheader";
+import Homebanner from "./components/homebanner";
+import Homenav from "./components/homenav";
 export default {
- 
+    components:{
+        "Homeheader-com":Homeheader,
+        "Homebanner-com":Homebanner,
+        "Homenav-com":Homenav
+    }
 }
 </script>
 
 
 <style lang="">
-    
+    .home{
+        width: 100%;
+        height:100%;
+    }
 </style>
