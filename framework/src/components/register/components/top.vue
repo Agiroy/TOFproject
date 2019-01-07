ae1192899a940ce59eb19cc6c3421bfbf3b75221
Your Name <you@example.com>
2019-01-07 14:18:40 +0800

    张伟修改注册

---
+++ framework/src/components/register/components/top.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="top">
+            <img src="../../../assets/icon_fh@2x.png" class="back">
+            <span>注册</span>
+            <a href="/login">登录</a>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    .top{
+        width: 100%;
+        height:0.88rem;
+        background:rgba(255,217,0,1);
+        display: flex;
+        justify-content: space-around;
+        align-items: center
+    }
+    .top .back{
+        width:0.17rem;
+        height:0.3rem;
+    }
+    .top span{
+        font-size:0.34rem;
+        font-family:PingFang-SC-Regular;
+        font-weight:400;
+        color:rgba(41,41,41,1);
+        line-height:0.15rem;
+    }
+    .top a{
+        font-size:0.24rem;
+        font-family:PingFang-SC-Regular;
+        font-weight:400;
+        color:rgba(41,41,41,1);
+        line-height:0.15rem;
+
+    }
+</style>
---
+++ framework/src/components/register/register.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="register">
+    <top-com/>
+    <main-com/>
+    </div>
+</template>
+<script>
+import Top from "./components/top"
+import Main from "./components/main"
+export default {
+    components:{
+        "top-com":Top,
+        "main-com":Main
+    }
+}
+</script>
+<style>
+.register{
+    width: 100%;
+    height: 100%;
+}
+</style>
