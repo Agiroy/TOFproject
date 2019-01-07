336b91f35f3ddfe97bdc3535d376aabe4df98998
zxySay <1374669917@qq.com>
2019-01-07 15:45:45 +0800

    zxyFirst

--- framework/src/App.vue
+++ framework/src/App.vue
@@ -18,6 +18,6 @@ export default {
 <style>
 #app {
   width: 100%;
-  height: 100%;
+  height: 100%;   
 }
 </style>
---
+++ framework/src/components/scart/components/top.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="top_bar">
+		<p>{{topname}}</p>
+	</div>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+				topname:"购物车"
+			}
+		}
+	}
+</script>
+
+<style>
+	.top_bar{
+		width: 100%;
+		height: 100%;
+		/*background: red;*/
+		border-bottom: 1px solid black;
+	    }
+	.top_bar>p{
+		color: black;
+		font-size: 0.48rem;
+		text-align: center;
+		line-height: .88rem;
+		}
+</style>
