d1376073e03a37ed2f5da8fd0f20bc27a7c070ba
heliling <1067253233@qq.com>
2019-01-07 15:58:35 +0800

    hll的

---
+++ framework/src/components/login/components/top.vue
@@ -0,0 +1,32 @@
+ <template>
+     <div class="top">
+	<img class="back" src="../../../assets/icon_fh@2x.png">
+<span class="title">登录</span>
+<a class="sig" href="/register">注册</a>
+     </div>
+ </template>
+ <script>
+ 
+ </script>
+ <style scoped>
+ .top{
+	width:100%;
+height:0.88rem;
+	background:#FFD900; display: flex;justify-content: space-around;align-items: center}
+    .top .back{width:0.17rem;
+height:0.3rem;}
+.title{ 
+font-size:0.34rem;
+ font-family:PingFang-SC-Regular;
+font-weight:400;
+color:rgba(41,41,41,1);
+line-height:0.15rem;}
+.sig{ 
+font-size:0.24rem;
+font-family:PingFang-SC-Regular;
+font-weight:400;
+color:rgba(41,41,41,1);
+line-height:0.15rem;}
+
+ </style>
+ 
