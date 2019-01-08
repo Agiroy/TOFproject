1d31d422ad44443cc0920b44e44cf0eb59c34f4f
sxx <sunxiangxino@163.com>
2019-01-08 10:25:30 +0800

    sxx3

--- framework/src/components/my/components/message/index.vue
+++ framework/src/components/my/components/message/index.vue
@@ -1,20 +1,29 @@
 <template>
     <div class="message">
-         <Header-com/>
+         <Header-com/>  
+         <Mes-com/>
     </div>
 </template>
 
 <script>
 
 import Header from './components/header';
+import Mes from './components/messageOne';
 
 export default {
-    components: {
-        "<Header-com>": Header
+    // namespace: true,
+
+    components:{
+        "Header-com": Header,
+        "Mes-com": Mes
     }
 }
 </script>
 
 <style lang="">
-    
+    .message{
+        width: 100%;
+        height: 100%;
+        background:#efefef;
+    }
 </style>
